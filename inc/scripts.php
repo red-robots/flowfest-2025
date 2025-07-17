e689bbba36020258cffca887805f3b583b0d8a31
Mindy <mindy@valhallaonlineservices.com>
2025-07-17 21:24:49 +0800

    Update script version

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,7 +3,7 @@
  * Enqueue scripts and styles.
  */
 function bellaworks_scripts() {
-	wp_enqueue_style( 'bellaworks-style', get_stylesheet_uri(), array(), '2.38' );
+	wp_enqueue_style( 'bellaworks-style', get_stylesheet_uri(), array(), '2.40' );
 
 	wp_deregister_script('jquery');
 		//wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, '1.10.2', true);
@@ -50,7 +50,7 @@ function bellaworks_scripts() {
 	wp_enqueue_script( 
 			'bellaworks-custom', 
 			get_template_directory_uri() . '/assets/js/custom.js', 
-			array(), '20250715', 
+			array(), '20250717', 
 			true 
 		);
 
