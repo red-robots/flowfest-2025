ca80f27bf457026d3ad8342c187a6fe871102129
Mindy <mindy@valhallaonlineservices.com>
2025-07-31 12:48:52 +0800

    Vendors to only pull from Vendors page affiliate

--- parts/content-vendors.php
+++ parts/content-vendors.php
@@ -1,37 +1,40 @@
 <!-- Sponsors -->
+asdsd
 <?php
-	if( have_rows("sponsors_footer", "option") ):
-		$sponsor_link = get_field('sponsor_link', 'option');
-        $sponsor_link_text = (isset($sponsor_link['title']) && $sponsor_link['title']) ? $sponsor_link['title'] : '';
-        $sponsor_link_url = (isset($sponsor_link['url']) && $sponsor_link['url']) ? $sponsor_link['url'] : '';
-        $sponsor_link_target = (isset($sponsor_link['target']) && $sponsor_link['target']) ? $sponsor_link['target'] : '_self';
+	$vendor_page = 1764;
+
+	if(have_rows('affiliate', $vendor_page)):
+		$vendor_link = get_field('vendor_link');
+        $vendor_link_text = (isset($vendor_link['title']) && $vendor_link['title']) ? $vendor_link['title'] : '';
+        $vendor_link_url = (isset($vendor_link['url']) && $vendor_link['url']) ? $vendor_link['url'] : '';
+        $vendor_link_target = (isset($vendor_link['target']) && $vendor_link['target']) ? $vendor_link['target'] : '_self';
 ?>
 	<section class="sponsors-container">
 		<div class="wrapper">
 			<h2 class="pagetitle text-center">Vendors</h2>
+								
 			<div class='sponsors owl-carousel owl-theme sponsors-loop'>
 
-				<?php while( have_rows("sponsors_footer", "option") ) : the_row();
+				<?php while(have_rows('affiliate', $vendor_page)): the_row();
+						$vendor_img = get_sub_field('affiliate_img', $vendor_page);
+						$vendor_url = get_sub_field('affiliate_url', $vendor_page); 
 
-					$sponsor_img = get_sub_field("sponsor_image");
-					$sponsor_url = get_sub_field("sponsor_url");
-
-					if ( !empty($sponsor_img) ) { ?>
+					if ( $vendor_img && $vendor_url ) { ?>
 						<div class="sponsor">
-							<?php if ( !empty($sponsor_url) ) { ?>
-								<a href="<?php echo $sponsor_url; ?>" target="_blank">
+							<?php if ( !empty($vendor_url) ) { ?>
+								<a href="<?php echo $vendor_url; ?>" target="_blank">
 							<?php } ?>
-								<img src="<?php echo $sponsor_img["url"]; ?>" alt="<?php echo $sponsor_img["alt"]; ?>" />
-							<?php if ( !empty($sponsor_url) ) { echo "</a>"; } ?>
+								<img src="<?php echo $vendor_img["url"]; ?>" alt="<?php echo $vendor_img["alt"]; ?>" />
+							<?php if ( !empty($vendor_url) ) { echo "</a>"; } ?>
 						</div>
 					<?php }
 
 				endwhile;
 			?>
 			</div>
-			<?php if($sponsor_link_text && $sponsor_link_url) { ?>
+			<?php if($vendor_link_text && $vendor_link_url) { ?>
 				<div class="text-center">
-					<a class="button-big" href="<?php echo $sponsor_link_url; ?>" target="<?php echo $sponsor_link_target; ?>"><?php echo $sponsor_link_text; ?></a>
+					<a class="button-big" href="<?php echo $vendor_link_url; ?>" target="<?php echo $vendor_link_target; ?>"><?php echo $vendor_link_text; ?></a>
 				</div>
 			<?php } ?>
 		</div>
